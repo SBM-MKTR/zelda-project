eb389d84cabe1cc274608f500aacd6c362d06c2b
Sohaib <sohaib.mokhtar-seddik@epfl.ch>
2026-03-01 14:10:06 +0100

    Fin section ajout des bush frontière, juste avant section Animation.

--- constants.py
+++ constants.py
@@ -7,5 +7,8 @@ SCALE = 2.0
 TILE_SIZE = 32
 """After scaling, the size of a tile."""
 
+PLAYER_MOVEMENT_SPEED = 4
+"""Speed of the player, in pixels per frame."""
+
 MAX_WINDOW_WIDTH = 14 * TILE_SIZE
 MAX_WINDOW_HEIGHT = 14 * TILE_SIZE
--- gameview.py
+++ gameview.py
@@ -16,6 +16,8 @@ class GameView(arcade.View):
     player_list: Final[arcade.SpriteList[arcade.Sprite]]
     grounds: Final[arcade.SpriteList[arcade.Sprite]]
     walls: Final[arcade.SpriteList[arcade.Sprite]]
+    physics_engine: Final[arcade.PhysicsEngineSimple]
+    camera: Final[arcade.camera.Camera2D]
 
     def __init__(self) -> None:
         # Magical incantion: initialize the Arcade view
@@ -46,13 +48,54 @@ class GameView(arcade.View):
 
         self.walls = arcade.SpriteList(use_spatial_hash=True)
         for x, y in [(3, 6), (7, 2), (2, 10), (3, 8)]:
-            bush = arcade.Sprite(
+            spr = arcade.Sprite(
                 TEXTURE_BUSH,
                 scale=SCALE,
                 center_x=grid_to_pixels(x),
                 center_y=grid_to_pixels(y),
             )
-            self.walls.append(bush)
+            self.walls.append(spr)
+        # Add bushes along the borders to prevent leaving the world
+        # Top border
+        for x in range(40):
+            spr = arcade.Sprite(
+                TEXTURE_BUSH,
+                scale=SCALE,
+                center_x=grid_to_pixels(x),
+                center_y=grid_to_pixels(19),
+            )
+            self.walls.append(spr)
+
+        # Bottom border
+        for x in range(40):
+            spr = arcade.Sprite(
+                TEXTURE_BUSH,
+                scale=SCALE,
+                center_x=grid_to_pixels(x),
+                center_y=grid_to_pixels(0),
+            )
+            self.walls.append(spr)
+
+        # Left border
+        for y in range(20):
+            spr = arcade.Sprite(
+                TEXTURE_BUSH,
+                scale=SCALE,
+                center_x=grid_to_pixels(0),
+                center_y=grid_to_pixels(y),
+            )
+            self.walls.append(spr)
+        # Right border
+        for y in range(20):
+            spr = arcade.Sprite(
+                TEXTURE_BUSH,
+                scale=SCALE,
+                center_x=grid_to_pixels(39),
+                center_y=grid_to_pixels(y),
+            )
+            self.walls.append(spr)
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player, self.walls)
+        self.camera = arcade.camera.Camera2D()
 
     def on_show_view(self) -> None:
         """Called automatically by 'window.show_view(game_view)' in main.py."""
@@ -65,6 +108,45 @@ class GameView(arcade.View):
     def on_draw(self) -> None:
         """Render the screen."""
         self.clear() # always start with self.clear()
-        self.grounds.draw()
-        self.walls.draw()
-        self.player_list.draw()
+        with self.camera.activate():
+             self.grounds.draw()
+             self.walls.draw()
+             self.player_list.draw()
+
+    def on_key_press(self, symbol: int, modifiers: int) -> None:
+        """Called when the user presses a key on the keyboard."""
+        match symbol:
+            case arcade.key.ESCAPE:
+                # redémarrer le jeu en recréant la vue
+                new_view = GameView()
+                self.window.show_view(new_view)
+            case arcade.key.RIGHT:
+                # start moving to the right
+                self.player.change_x = +PLAYER_MOVEMENT_SPEED
+            case arcade.key.LEFT:
+                # start moving to the left
+                self.player.change_x = -PLAYER_MOVEMENT_SPEED
+            case arcade.key.UP:
+                # start moving upwards
+                self.player.change_y = +PLAYER_MOVEMENT_SPEED
+            case arcade.key.DOWN:
+                # start moving downwards
+                self.player.change_y = -PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, symbol: int, modifiers: int) -> None:
+        """Called when the user releases a key on the keyboard."""
+        match symbol:
+            case arcade.key.RIGHT | arcade.key.LEFT:
+                # stop horizontal movement
+                self.player.change_x = 0
+            case arcade.key.UP | arcade.key.DOWN:
+                # stop vertical movement
+                self.player.change_y = 0
+
+    def on_update(self, delta_time: float) -> None:
+        """Called once per frame, before drawing.
+
+        This is where in-world time "advances", or "ticks".
+        """
+        self.physics_engine.update()
+        self.camera.position = self.player.position
