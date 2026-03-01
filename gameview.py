86358aef6e3bd52b2b5a886870e2e1549a676b1a
Sohaib <sohaib.mokhtar-seddik@epfl.ch>
2026-03-01 13:07:46 +0100

    Fin de section Dessiner des objets : les Sprites.

--- gameview.py
+++ gameview.py
@@ -2,12 +2,20 @@ from typing import Final
 import arcade
 
 from constants import *
+from textures import *
+
+def grid_to_pixels(i : int) ->int:
+    return i * TILE_SIZE + (TILE_SIZE // 2)
 
 class GameView(arcade.View):
     """Main in-game view."""
 
     world_width: Final[int]
     world_height: Final[int]
+    player: Final[arcade.Sprite]
+    player_list: Final[arcade.SpriteList[arcade.Sprite]]
+    grounds: Final[arcade.SpriteList[arcade.Sprite]]
+    walls: Final[arcade.SpriteList[arcade.Sprite]]
 
     def __init__(self) -> None:
         # Magical incantion: initialize the Arcade view
@@ -19,6 +27,32 @@ class GameView(arcade.View):
         # Setup our game
         self.world_width = 40 * TILE_SIZE
         self.world_height = 20 * TILE_SIZE
+        self.player = arcade.Sprite(
+            TEXTURE_PLAYER_IDLE_DOWN,
+            scale=SCALE, center_x=grid_to_pixels(2), center_y=grid_to_pixels(2)
+        )
+        self.player_list = arcade.SpriteList()
+        self.player_list.append(self.player)
+        self.grounds = arcade.SpriteList(use_spatial_hash=True)
+        for x in range(40):
+            for y in range(20):
+                grass = arcade.Sprite(
+                    TEXTURE_GRASS,
+                    scale=SCALE,
+                    center_x=grid_to_pixels(x),
+                    center_y=grid_to_pixels(y),
+                )
+                self.grounds.append(grass)
+
+        self.walls = arcade.SpriteList(use_spatial_hash=True)
+        for x, y in [(3, 6), (7, 2), (2, 10), (3, 8)]:
+            bush = arcade.Sprite(
+                TEXTURE_BUSH,
+                scale=SCALE,
+                center_x=grid_to_pixels(x),
+                center_y=grid_to_pixels(y),
+            )
+            self.walls.append(bush)
 
     def on_show_view(self) -> None:
         """Called automatically by 'window.show_view(game_view)' in main.py."""
@@ -31,3 +65,6 @@ class GameView(arcade.View):
     def on_draw(self) -> None:
         """Render the screen."""
         self.clear() # always start with self.clear()
+        self.grounds.draw()
+        self.walls.draw()
+        self.player_list.draw()
