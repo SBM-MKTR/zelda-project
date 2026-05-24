efc24437d5478836c1b6aafcd92e061d85d2eeef
Sohaib <sohaib.mokhtar-seddik@epfl.ch>
2026-05-24 14:28:38 +0200

    Gros refactoring sur tout le map avec modif sur dossier Map

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from constants import *
 from gameview import GameView
 from map import *
 
-DEFAULT_MAP_PATH = "maps/map1.txt"
+DEFAULT_MAP_PATH = "Maps/map1.txt"
 
 def main() -> None:
     if len(sys.argv) == 1:
